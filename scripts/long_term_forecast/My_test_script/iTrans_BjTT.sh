751f097abdd687b8aaba8d4fdae227e33b96f1b6
r0oup1iao <yanhaoyang99@126.com>
2024-12-20 15:46:07 +0800

    fix bugs

--- scripts/long_term_forecast/My_test_script/iTrans_BjTT.sh
+++ scripts/long_term_forecast/My_test_script/iTrans_BjTT.sh
@@ -1,6 +1,6 @@
-export CUDA_VISIBLE_DEVICES="0"
+export CUDA_VISIBLE_DEVICES="0, 1"
 
-model_name=DLinear
+model_name=iTransformer
 num_workers=10
 enc_in=1063
 
@@ -16,15 +16,20 @@ python -u run.py \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 5 \
-  --e_layers 2 \
+  --e_layers 4 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in $enc_in \
-  --dec_in 7 \
-  --c_out 7 \
+  --enc_in $enc_in\
+  --dec_in $enc_in\
+  --c_out $enc_in\
   --des 'Exp' \
   --inverse \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
   --num_workers $num_workers \
+  --use_multi_gpu \
   --itr 1
 
 python -u run.py \
@@ -39,15 +44,20 @@ python -u run.py \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 10 \
-  --e_layers 2 \
+  --e_layers 4 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in $enc_in \
-  --dec_in 7 \
-  --c_out 7 \
+  --enc_in $enc_in\
+  --dec_in $enc_in\
+  --c_out $enc_in\
   --des 'Exp' \
   --inverse \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
   --num_workers $num_workers \
+  --use_multi_gpu \
   --itr 1
 
 python -u run.py \
@@ -62,13 +72,18 @@ python -u run.py \
   --seq_len 96 \
   --label_len 48 \
   --pred_len 15 \
-  --e_layers 2 \
+  --e_layers 4 \
   --d_layers 1 \
   --factor 3 \
-  --enc_in $enc_in \
-  --dec_in 7 \
-  --c_out 7 \
+  --enc_in $enc_in\
+  --dec_in $enc_in\
+  --c_out $enc_in\
   --des 'Exp' \
   --inverse \
+  --d_model 512 \
+  --d_ff 512 \
+  --batch_size 16 \
+  --learning_rate 0.001 \
   --num_workers $num_workers \
+  --use_multi_gpu \
   --itr 1
